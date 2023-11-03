7a702852167712a5c32b1cf18ccac8f40643927c
Emily ORiordan <emily@bodekerscientific.com>
2023-11-03 16:14:56 +1300

    First commit

---
+++ display.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def add_caption_to_df(df, caption, color='black', fontsize=100):
+    """Mainly to be used for display in Jupyter notebooks."""
+    styles = [dict(selector="caption",
+                       props=[("text-align", "center"),
+                              ("font-size", "100%"),
+                              ("color", 'white')])]
+
+    df = df.style.set_caption(caption).set_table_styles(styles)
+
+    return df
