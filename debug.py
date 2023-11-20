6abf272432656e2b7d9046962f64b294ce69cff2
Emily ORiordan <emily@bodekerscientific.com>
2023-11-20 13:24:40 +1300

    Add more plotting functions

--- debug.py
+++ debug.py
@@ -4,11 +4,18 @@ import matplotlib.pyplot as plt
 def sns_plot_and_save(data, path='fig.png'):
     fig, ax = plt.subplots()
     sns.heatmap(data, ax=ax)
+    ax.invert_yaxis()
     fig.savefig(path)
     plt.close(fig)
 
 def hist_plot_and_save(data, path='fig.png',  bins=50):
     fig, ax = plt.subplots()
     ax.hist(data, bins=bins)
+    fig.savefig(path)
+    plt.close(fig)
+
+def plt_plot_and_save(data, path='fig.png'):
+    fig, ax = plt.subplots()
+    ax.imshow(data, cmap="viridis")
     fig.savefig(path)
     plt.close(fig)
