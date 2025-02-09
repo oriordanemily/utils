637158cec8a0ca632b78bc9aa6246f7e789ff3cf
oriordanemily <emily@bodekerscientific.com>
2025-02-10 09:57:56 +1300

    Add kwargs to functions

--- debug.py
+++ debug.py
@@ -1,21 +1,22 @@
 import seaborn as sns
 import matplotlib.pyplot as plt 
 
-def sns_plot_and_save(data, path='fig.png'):
+def sns_plot_and_save(data, path='fig.png', invert_axis=True, **kwargs):
     fig, ax = plt.subplots()
-    sns.heatmap(data, ax=ax)
-    ax.invert_yaxis()
-    fig.savefig(path)
+    sns.heatmap(data, ax=ax, **kwargs)
+    if invert_axis:
+        ax.invert_yaxis()
+    fig.savefig(path, bbox_inches=True)
     plt.close(fig)
 
-def hist_plot_and_save(data, path='fig.png',  bins=50):
+def hist_plot_and_save(data, path='fig.png', bins=50, **kwargs):
     fig, ax = plt.subplots()
-    ax.hist(data, bins=bins)
-    fig.savefig(path)
+    ax.hist(data, bins=bins, **kwargs)
+    fig.savefig(path, bbox_inches=True)
     plt.close(fig)
 
-def plt_plot_and_save(data, path='fig.png'):
+def plt_plot_and_save(data, path='fig.png', **kwargs):
     fig, ax = plt.subplots()
-    ax.imshow(data, cmap="viridis")
-    fig.savefig(path)
+    ax.imshow(data, cmap="viridis", **kwargs)
+    fig.savefig(path, bbox_inches=True)
     plt.close(fig)
