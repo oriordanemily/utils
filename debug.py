7f81e09effbc6738855960328a2511304a1730f1
oriordanemily <emily@bodekerscientific.com>
2025-02-10 10:13:01 +1300

    Small fix

--- debug.py
+++ debug.py
@@ -6,17 +6,17 @@ def sns_plot_and_save(data, path='fig.png', invert_axis=True, **kwargs):
     sns.heatmap(data, ax=ax, **kwargs)
     if invert_axis:
         ax.invert_yaxis()
-    fig.savefig(path, bbox_inches=True)
+    fig.savefig(path, bbox_inches='tight')
     plt.close(fig)
 
 def hist_plot_and_save(data, path='fig.png', bins=50, **kwargs):
     fig, ax = plt.subplots()
     ax.hist(data, bins=bins, **kwargs)
-    fig.savefig(path, bbox_inches=True)
+    fig.savefig(path, bbox_inches='tight')
     plt.close(fig)
 
 def plt_plot_and_save(data, path='fig.png', **kwargs):
     fig, ax = plt.subplots()
     ax.imshow(data, cmap="viridis", **kwargs)
-    fig.savefig(path, bbox_inches=True)
+    fig.savefig(path, bbox_inches='tight')
     plt.close(fig)
